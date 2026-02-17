797b5ab8f7b8868e55db343aff363e7ffd6dfde3
ashuksu <ashuksu@gmail.com>
2026-02-17 09:56:30 +0100

    add TypeScript generic functions and interfaces for enhanced type safety

---
+++ src/index-3.1.ts
@@ -0,0 +1,82 @@
+function identity<T>(value: T): T {
+    return value
+}
+
+// const a = identity<string>("hello")
+// const b = identity<number>(42)
+// const c = identity(true)
+
+
+// console.log(11, a);
+// console.log(22, b);
+// console.log(33, c);
+
+function getFirstElement<T>(arr: T): T | undefined {
+    // if (!!arr.length) return;
+
+    return arr;
+}
+
+// const n = getFirstElement([1, 'a']);
+// const n = getFirstElemen([1, 2, 3]);
+// const s = getFirstElement(['a', 'b', 'c']);
+
+// console.log(44, n);
+// console.log(55, s);
+
+function logLengt<T extends { length: number }>(value: T): void {
+    console.log(value.length);
+}
+
+// logLengt('hello');
+// logLengt([1, 2, 3]);
+function wrap<T>(value: T) {
+    return {value}
+}
+
+const result = wrap(123)
+
+// console.log(result)
+
+function createPair<T, U>(first: T, second: U): {
+    first: T,
+    second: U
+} {
+    return {
+        first: first,
+        second: second
+    }
+}
+
+function createPair2<T, U>(first: T, second: U) {
+    return {first, second}
+}
+
+console.log(createPair("age", 230))
+console.log(createPair2("age", 230))
+
+interface User {
+    id: number,
+    email: string,
+    role: 'admin' | 'user'
+    isActive: boolean
+}
+
+interface ApiResponse<T> {
+    data: T,
+    status: number
+}
+
+const userResponse: ApiResponse<User> = {
+    data: {
+        id: 1,
+        email: 'a@mail.com',
+        role: 'admin',
+        isActive: true
+    },
+    status: 200
+}
+
+
+
+
---
+++ src/index-3.2.ts
@@ -0,0 +1,66 @@
+// # Generic-function
+
+// works with different types.
+// It is logically universal.
+
+
+function reeat<T>(value: T, times: number): T[] {
+    const result: T[] = [];
+
+    for (let i = 0; i < times; i++) {
+        result.push(value);
+    }
+
+    console.log(result);
+    return result;
+}
+
+
+// const numbers = reeat(5, 3); // numbers → number[]
+// const strings = reeat('hi', 2); // strings → string[]
+
+
+
+function identity<T>(value: T): T {
+    return value;
+}
+
+
+// # Generic-interface
+
+
+interface Box<T> {
+    value: T
+}
+
+const numberBox: Box<number> = {
+    value: 10, // Box<number> → value: number
+}
+
+const stringBox: Box<string> = {
+    value: 'hello' // Box<string> → value: string
+}
+
+
+// ---------------------
+
+
+interface ApiResult<T> {
+    data: T
+    error: string | null
+}
+
+
+function fetchData<T>(data: T): ApiResult<T> {
+    return {
+        data,
+        error: null
+    }
+}
+
+// ---------------------
+
+
+const result2 = fetchData(['a', 'b']); // result: ApiResult<string[]>, T = string[]
+
+// console.log(result2); // result2.data → string[]
